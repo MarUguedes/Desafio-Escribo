0ff5b19ca481b8ff8581f0c40d2652c227e9794a
Marcela Guedes <marcellauguedes@gmail.com>
2022-01-25 10:16:09 -0300

    adicionei o arquivo dart

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  print("Digite um valor");
+  num valor = num.parse(stdin.readLineSync() ?? "0");
+
+  somaValores(valor);
+}
+
+void somaValores(num valor) {
+  var soma = 0;
+
+  for (var i = 1; i < valor; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      soma += i;
+    }
+  }
+  print("A soma de todos os valores inteiros divisiveclsis por 3 ou 5 e menor que $valor é: $soma");
+}
