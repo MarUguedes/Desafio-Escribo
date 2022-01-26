133cfa13ba3715692e0b8fa5a4e2825d373ef659
Marcela Guedes <marcellauguedes@gmail.com>
2022-01-25 21:21:10 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,19 +1,17 @@
 import 'dart:io';
 
 void main() {
-
   print('\nBem-vindo (a) ao desafio Escribo\n');
   var menu = 1;
 
   while (menu != 2) {
-
     print("Digite um número que deseja somar");
     num valor = num.parse(stdin.readLineSync() ?? '');
 
     //Chamada da função que executa o calculo
     somaValores(valor);
 
-    print("\nDigite 1 se deseja continuar ou 2 para encerrar a aplicação");
+    print("\nDigite 1 para testar outro valor ou 2 para encerrar a aplicação");
     menu = int.parse(stdin.readLineSync() ?? '');
     print(menu != 2 && menu != 1 ? "\nOpção inválida" : "");
   }
@@ -21,17 +19,22 @@ void main() {
   print("Fim da aplicação");
 }
 
-
 //Função para somar os valores dentro da condição dada
 
 void somaValores(num valor) {
   var soma = 0;
 
-  for (var i = 1; i < valor; i++) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      soma += i;
+  if (valor < 0) {
+
+    print("Digite um valor maior que 0");
+
+  } else {
+    for (var i = 1; i < valor; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        soma += i;
+      }
     }
+    print(
+        "\nSomatório de todos os valores inteiros divisíveis por 3 ou 5 que sejam inferiores a $valor é: $soma");
   }
-  print(
-      "\nSomatório de todos os valores inteiros divisíveis por 3 ou 5 que sejam inferiores a $valor é: $soma");
 }
